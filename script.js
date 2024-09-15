89c33c5b0a7157a818b0239a78774a61319871cd
edgz98 <edgz98@hotmail.com>
2024-09-15 17:18:36 +0900

    Functioning Pokemon Image retriever by searching through Pokemon Name

---
+++ script.js
@@ -0,0 +1,32 @@
+async function fetchData() {
+  try {
+    const pokemonName = document
+      .getElementById("pokemonName")
+      .value.toLowerCase();
+
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Could not fetch resource");
+    }
+
+    const data = await response.json();
+    const pokemonSprite = data.sprites.front_default;
+    const imgElement = document.getElementById("pokemonsprite");
+
+    imgElement.src = pokemonSprite;
+    imgElement.style.display = "block";
+  } catch (error) {
+    console.error("Error", error);
+  }
+}
+
+document
+  .getElementById("pokemonName")
+  .addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      document.getElementById("searchBtn").click();
+    }
+  });
